b467ed3c04358f44de4b2d3d2421236e0c941a56
Espen Sivertsen Oftedal <98535233+Essivoft@users.noreply.github.com>
2024-05-14 16:29:42 +0200

    Update __init__.py

--- ckanext/my_landing_page/controller/__init__.py
+++ ckanext/my_landing_page/controller/__init__.py
@@ -3,7 +3,8 @@ from ckan.plugins.toolkit import BaseController, render
 
 class LandingPageController(BaseController):
     def index(self):
-        context = {'model': tk.model, 'session': tk.session, 'user': tk.c.user}
+        # Bruk et tomt kontekstobjekt
+        context = {}
         data_dict = {'all_fields': True, 'sort': 'name asc', 'limit': 20}
         groups = tk.get_action('group_list')(context, data_dict)
         return render('landing_page.html', extra_vars={'groups': groups})
